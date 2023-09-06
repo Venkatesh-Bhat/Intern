b87aace495167a4627d13dec9416afd10442115c
B-u-n-d-a <venkateshbhat71@gmail.com>
2023-09-06 12:14:33 +0530

    Function with Parameters

--- JavaScript/sample.js
+++ JavaScript/sample.js
@@ -1,10 +1,17 @@
-let a = window.prompt("Enter age");
-function checkAge() {
-  if (a < 15) {
-    window.alert("Hes innocent");
-  } else {
-    window.alert("He's notorious");
-  }
+// let a = window.prompt("Enter age");
+// function checkAge() {
+//   if (a < 15) {
+//     window.alert("Hes innocent");
+//   } else {
+//     window.alert("He's notorious");
+//   }
+// }
+
+// checkAge();
+
+let name = "Mike Tyson";
+function helloBro(nameArg) {
+  window.alert(`Hello ${nameArg}`);
 }
 
-checkAge();
+helloBro(name);
