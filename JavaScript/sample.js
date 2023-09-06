4e1f40216662a830e595a0d06dfa35065e859d2c
B-u-n-d-a <venkateshbhat71@gmail.com>
2023-09-06 12:12:02 +0530

    Function commit

---
+++ JavaScript/sample.js
@@ -0,0 +1,10 @@
+let a = window.prompt("Enter age");
+function checkAge() {
+  if (a < 15) {
+    window.alert("Hes innocent");
+  } else {
+    window.alert("He's notorious");
+  }
+}
+
+checkAge();
