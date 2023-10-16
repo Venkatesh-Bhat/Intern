eee818b47486908ffce8e714cb50674ece3dbab3
B-u-n-d-a <venkateshbhat71@gmail.com>
2023-10-16 07:40:41 +0530

    Fixing History Bug

--- tictactoe/src/components/game.js
+++ tictactoe/src/components/game.js
@@ -11,9 +11,17 @@ export default function Game() {
   function handlePlay(newBoard, row, col) {
     const nextHistory = [...history.slice(0, currentMove + 1), newBoard];
     setHistory(nextHistory);
-    // const newRowCols = [...rowCols, `(${row + 1} , ${col + 1})`];
-    // setRowCols(newRowCols);
-    setRowCols((prevRowCols) => [...prevRowCols, `(${row + 1}, ${col + 1})`]);
+    console.log(nextHistory);
+    const newRowCols = [
+      ...rowCols.slice(0, currentMove),
+      `(${row + 1} , ${col + 1})`,
+    ];
+    console.log(newRowCols);
+    setRowCols(newRowCols);
+    // setRowCols((prevRowCols) => [
+    //   ...prevRowCols.slice(0, currentMove + 1),
+    //   `(${row + 1}, ${col + 1})`,
+    // ]);
     setCurrentMove(nextHistory.length - 1);
   }
 
@@ -22,13 +30,13 @@ export default function Game() {
   }
 
   const moves = history.map((squares, move) => {
-    let description =
-      move > 0 ? `Go to move #${move} ${rowCols[move]}` : "Go to start game";
-
+    // let description =
+    //   move > 0 ? `Go to move #${move} ${rowCols[move]}` : "Go to start game";
+    let description = `Go to move #${move + 1} ${rowCols[move]}`;
     return (
       <li key={move}>
         {move === currentMove ? (
-          `You are at move #${move}`
+          `You are at move #${move + 1}`
         ) : (
           <button onClick={() => jumpTo(move)}>{description}</button>
         )}
